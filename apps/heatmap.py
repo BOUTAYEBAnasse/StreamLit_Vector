19f1c6fd6613f564e8288d38905160f2646461dd
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 11:44:25 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -6,10 +6,23 @@ def app():
 
     st.title("Get the nearest point")
 
+    with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+    st.write("Outside the form")
+
+
     #random_column = st.selectbox("Select a column to apply random colors", column_names)
 
 
-    user_input = st.text_input("label goes here", default_value_goes_here)
+
 
 
     layer='./GBR_rrd/GBR_rails.shp'
