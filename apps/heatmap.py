f8065f3264f2a732866d081203ca33a74bb96303
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 12:02:15 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -11,9 +11,14 @@ def app():
 
     form = st.form("my_form")
 
-    # Now add a submit button to the form:
 
-    title = st.text_input('Movie title', 'Life of Brian')
+
+    longitude = st.text_input('Longitude :', '')
+    lattitude=  st.text_input('Lattitude :', '')
+
+
+    #Form :
+
     st.write('The current movie title is', title)
     form.form_submit_button("Submit")
 
