6c0603c5a8fd52166fd64d36369240d641045645
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 02:11:48 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -6,15 +6,11 @@ def app():
 
     st.title("Heatmap")
 
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-    m = leafmap.Map(tiles="stamentoner")
-    m.add_heatmap(
-        filepath,
-        latitude="latitude",
-        longitude="longitude",
-        value="pop_max",
-        name="Heat map",
-        radius=20,
-    )
+    m = leafmap.Map(center=[0, 0], zoom=2)
+
+    in_shp = 'C:\Users\Lenovo\Desktop\GBR_rrd\GBR_rails.shp'
+    m.add_shp(in_shp, layer_name="Railways")
+
+    #filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
     m.to_streamlit(height=700)
 
