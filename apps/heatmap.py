41e5ca418a0f5aebeeaad2930e5f7c89882775a3
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 12:21:23 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -2,6 +2,38 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 
 
+
+def calculate_distance_view(request):
+    form = addressModelForm(request.POST or None)
+    glocator = Nominatim(user_agent='reporter')
+    model = Services
+    context_object_name = "services"
+    m = folium.Map(width=800, height=500, location=user_location_init, zoom_start=10)
+
+    if form.is_valid():
+         user_address = form.cleaned_data.get('user_location')
+         user_location = glocator.geocode(user_address)
+         d_lat = user_location.latitude
+         d_long = user_location.longitude
+         current_location = Point(d_lat, d_long, srid=4326)
+         nearbyobj = Services.objects.annotate(distance=Distance("location", current_location)).order_by("distance")[0:3]
+
+         m = folium.Map(width=800, height=500, location=user_location_init, zoom_start=10)
+         folium.Marker([d_lat, d_long], tooltip='click here for more', popup=user_location,
+                       icon=folium.Icon(color='red', icon='cloud')).add_to(m)
+
+
+    m = m._repr_html_()
+
+    context = {
+        'form': form,
+        'model': model,
+        'nearbyobj': 'nearbyobj',
+        'map': m
+    }
+    return render(request, 'index.html', context)
+
+
 def app():
 
     st.title("Get the nearest point")
@@ -14,8 +46,8 @@ def app():
  
 
 
-    form.text_input('Longitude : ', '')
-    form.text_input('Lattitude : ', '')
+    longitude_value=form.text_input('Longitude : ', '')
+    lattitude_value=form.text_input('Lattitude : ', '')
     form.form_submit_button("Recherecher le point le plus proche")
 
 
