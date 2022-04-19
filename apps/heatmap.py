e27bae7c2fa38f38d354d86a039db7f24817f08a
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 02:15:16 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -6,11 +6,6 @@ def app():
 
     st.title("Heatmap")
 
-    #m = leafmap.Map(center=[0, 0], zoom=2)
-
-    #in_shp = 'C:\Users\Lenovo\Desktop\GBR_rrd\'
-    #m.add_shp(in_shp, layer_name="Countries")
-
     filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
     m = leafmap.Map(tiles="stamentoner")
     m.add_heatmap(
@@ -22,4 +17,3 @@ def app():
         radius=20,
     )
     m.to_streamlit(height=700)
-
