a16eaf1d3b4f035498aa6e98185b1693abb2d61e
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 02:23:44 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -4,13 +4,13 @@ import leafmap.foliumap as leafmap
 
 def app():
 
-    st.title("Heatmap")
+    #st.title("Heatmap")
 
     #chemin="C:\Users\Lenovo\Desktop\GBR_rrd\GBR_rails.shp"
 
     chemin='../GBR_rrd/GBR_rails.shp'
 
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    #filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
     m = leafmap.Map(tiles="stamentoner")
     m.add_shp(chemin, layer_name="Railways")
     m.to_streamlit(height=700)
