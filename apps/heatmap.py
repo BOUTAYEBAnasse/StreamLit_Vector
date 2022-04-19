e2b52844f1e120a1b73dbe1a1b93acfcea5142bc
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 11:54:40 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -10,8 +10,6 @@ def app():
 
 
     form = st.form("my_form")
-    form.slider("Inside the form")
-    st.slider("Outside the form")
 
     # Now add a submit button to the form:
     form.form_submit_button("Submit")
