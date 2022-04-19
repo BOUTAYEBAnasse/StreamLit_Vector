11b2fedeba69a54308399979a8a48ca4361587b6
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 12:12:03 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,18 +30,6 @@ with st.sidebar:
         default_index=default_index,
     )
 
-    st.sidebar.title("About")
-    st.sidebar.info(
-        """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
-
-        More menu icons: <https://icons.getbootstrap.com>
-    """
-    )
-
 for app in apps:
     if app["title"] == selected:
         app["func"]()
