ad7bcacc8212b1cfbf8db4f51623ad6bb314b071
BOUTAYEBAnasse <me@mydomain.com>
2022-04-19 03:19:09 +0000

    change

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -10,7 +10,7 @@ def app():
     chemin='./GBR_rrd/GBR_rails.shp'
 
 
-    m = leafmap.Map(center=[0, 0], zoom=2)
+    m = leafmap.Map(center=[-1.077901,52.126744], zoom=10)
     #m.add_shp(chemin, layer_name="Railways")
     #m.to_streamlit(height=700)
 
